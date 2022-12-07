5db3ca16dc02dc3519496fdef88bbd05dd98d2e5
hpweisberg <hpweisberg@gmail.com>
2022-12-06 17:07:22 -0800

    add empty button feature and alert

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 const inputBox = document.getElementById('myInput')
 const btn = document.getElementById('submit-button')
 const myTodoList = document.getElementById('todo-list')
-console.log(inputBox)
+console.log(myInput)
 
 btn.addEventListener('click', function(evt){
   const li = document.createElement('li')
-  const inp = document.querySelector('input')
-  li.textContent = inp.value
-  document.getElementById('todo-list').appendChild(li)
-  inp.value = ''
+  const inp = document.getElementById('myInput')
+    if(myInput.value.trim()){
+      li.textContent = inp.value
+      document.getElementById('todo-list').appendChild(li)
+      inp.value = ''
+    } else {
+      alert('Please enter a To-do item')
+  }
 })
