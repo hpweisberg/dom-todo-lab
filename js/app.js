f2d16b1c785498a6fb3adc1cf1e27e01dec51101
hpweisberg <hpweisberg@gmail.com>
2022-12-06 16:24:43 -0800

    add 3 cashed element references

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const inputBox = document.getElementById('myInput')
+const btn = document.getElementById('submit-button')
+const myTodoList = document.getElementById('todo-list')
+console.log(inputBox)
