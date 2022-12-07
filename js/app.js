bb14793abb72a788141bf2a1b170c4e757d3dbfa
hpweisberg <hpweisberg@gmail.com>
2022-12-06 16:33:58 -0800

    add button event listeners to create list and clear input value

--- js/app.js
+++ js/app.js
@@ -2,3 +2,11 @@ const inputBox = document.getElementById('myInput')
 const btn = document.getElementById('submit-button')
 const myTodoList = document.getElementById('todo-list')
 console.log(inputBox)
+
+btn.addEventListener('click', function(evt){
+  const li = document.createElement('li')
+  const inp = document.querySelector('input')
+  li.textContent = inp.value
+  document.getElementById('todo-list').appendChild(li)
+  inp.value = ''
+})
