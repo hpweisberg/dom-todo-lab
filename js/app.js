d8c0b2b046f1074e2e83a30ac85ea4e99ed4fa1b
hpweisberg <hpweisberg@gmail.com>
2022-12-06 18:29:35 -0800

    add comments to code

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
+// Cashed element references:
 const inputBox = document.getElementById('myInput')
 const btn = document.getElementById('submit-button')
 const myTodoList = document.getElementById('todo-list')
-console.log(myInput)
 const resetBtn = document.getElementById('reset-button')
 
+// Add To-do button:
 btn.addEventListener('click', function(evt){
   const li = document.createElement('li')
   const inp = document.getElementById('myInput')
@@ -16,11 +17,12 @@ btn.addEventListener('click', function(evt){
   }
 })
 
+// Add Reset button:
 resetBtn.addEventListener('click', function(){
   [...myTodoList.children].forEach(c => c.remove())
 })
 
-
+// Add remove indivual line items
 myTodoList.addEventListener('click', function(evt){
   evt.target.remove()
   console.log(evt.target)
